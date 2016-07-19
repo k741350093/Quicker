ca8cdf60cd6ad94fd508333cd422f273f958b42e
k741350093 <741350093@qq.com>
2016-07-19 11:32:17 +0800

    Quicker test commit.

--- src/com/quicker/tools/StaticConstant.java
+++ src/com/quicker/tools/StaticConstant.java
@@ -1,6 +1,6 @@
 package com.quicker.tools;
 
 public class StaticConstant {
-	public static String localhostConstant = "119.29.175.106";
+	public static String localhostConstant = "119.29.175.100";
 	public static String stuIdConstant;
 }
